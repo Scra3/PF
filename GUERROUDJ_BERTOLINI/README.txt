4365c546e092fde1a36d53e713244ef93ac9827b
scra3 <albanbertolini@gmail.com>
2016-11-30 23:57:49 +0100

    end

---
+++ GUERROUDJ_BERTOLINI/README.txt
@@ -0,0 +1,4 @@
+GROUPE : MR Guerroudj MR BERTOLINI
+
+Question 1,2,3 résolu 
+il manque la question 4 et l'exécutable
--- david.hs
+++ david.hs
@@ -1,103 +1,58 @@
-import Data.IORef
-import Data.Maybe
-import Data.List
-import Data.Char
---import Test.QuickCheck
-import System.IO.Unsafe
-import qualified Data.Map as Map
+--codeOf
+	codeOf (APREF []) lettres = Nothing
 
--- Synonyme Code
-type Code = Int
-
--- Arbre Prefix
-data APREF = APREF [(Char,Code,APREF)] deriving (Show,Eq)
--- Structure de type exemple Sal, Sb : [('s',1,[('a',2,[('l',3,[])],('b',2,[])])]
--- Description de l'algorithme
--- Si la liste est vide alors créer une succession de tableau et remplir le premier élements des tableau par la lettre i du mot juqu a n 
--- Sinon Regarder si lettre i existe : si elle existe regarder dans son arbre de sufixe si l'élément i +1 existe juqu'à ne pas trouver l'élément et le rajouter
-					-- Si il existe pas le créer et revenir au ca si tableau vide
-
--- Pour calculer le code il suffira de faire la concatenation des indices des lettres dans les tableaux.
--- POur calculer la chaine il suffit de parcourir les chaines
-class Table a where
-	empty :: a 
-	ajouter :: a -> String -> a
-	codeOf :: a -> String -> Maybe Code
-	stringOf :: a -> Code -> Maybe String
-	isIn :: a -> String -> Bool
-	split :: a -> String -> (String ,Maybe Code ,String)
-	
-	
-test = APREF[('a',0,APREF[])]
-
-instance Table APREF where
--------------
--- Empty   --
--------------
-
-	empty =  APREF[('~',-1,APREF[])] 
-
--------------
--- Ajouter --
--------------
-
-	-- on envoit rien a un arbre vide -> return empty
-	ajouter (APREF[]) [] = empty
-
-	-- on envoit un element à un arbre vide
-	ajouter (APREF[]) (x:xs) = ajouter(empty)(x:xs)
-
-	-- on envoit un element a un arbre
---	ajouter (APREF[(char,code, arbreFils)]) (x:xs) = case code of
-		-- si le code = -1 on est en haut, alors on ajoute un noeud
---		(-1) -> ajouter (APREF[(x,(code*10+1), APREF[])]) xs
---		_ -> if char = x
---		then (APREF[(char,code*10+1, arbreFils)])
-		-- sinon on concatene		
---		else (APREF[(char,code, arbreFils):(char,code, arbreFils])
---		where
---			code = 
---			char =
-
-	-- si on a qu'un seul élement/lettre
---	ajouter (APREF[(char,code,arbrefils)]) x == case code of
-		-- si le code = -1 on est en haut, alors on ajoute un noeud
---		(-1) -> ajouter (APREF[(x,(code*10+1), APREF[])]) xs
-		-- on regarde le caractere		
---		_ -> case x of
-			-- s'il est vide
---			[] -> 
-			--sinon
---			_ -> if x = char
-			--si le caractere est le meme on descend dans l'arbre
---			then
-			--sinon on cree un noeud
---			else
---		where
---			code = -1
---			char =
+	-- Si on chercher seulement une lettre dans la liste des fils et qu'on la trouve on envoit son Just code sinon nothing
+	codeOf (APREF(ar:ars)) (l:[]) = if ([find (ar:ars) l]) == []
+				then Nothing
+				else Just $ return (ar:ars) l
+		where 
+			find (ar:ars) letter = head $ filter (\(a,b,c) -> letter==a) (ar:ars)
+			getCode (a,b,c) = b
+			return (ar:ars) l = (\a -> 10 * a) $ getCode $ find (ar:ars) l
 
 
-------------------------------
--- Fonctions additionnelles --
-------------------------------
-
--- On return le code d'un APREF			
-getcode :: APREF -> Code
-getcode (APREF[]) = (-1)
-getcode (APREF[( _ , code, _ )]) = code
-
--- On return le char d'un APREF	
-getchar :: APREF -> Char
-getchar (APREF[]) = '~'
-getchar (APREF[( char , _ , _ )]) = char
-
---On fet le fils
-getFils :: APREF -> APREF
-getFils (APREF[]) = APREF[]
-getFils (APREF[( _ , _ , arbreFils)]) =  arbreFils
+	codeOf (APREF(ar:ars)) (l:ls) = if ([find (ar:ars) l]) == []
+						then Nothing
+						else Just $ call (ar:ars) l
+			where 
+				find (ar:ars) letter = head $ filter (\(a,b,c) -> letter==a) (ar:ars)
+				getCode (a,b,c) = b
+				getNext (a,b,c) = c
+				return (ar:ars) l = (\a -> 10 * a) $ getCode $ find (ar:ars) l	
+				call (ar:ars) l = fromJust (codeOf (getNext $ find (ar:ars) l) ls)					  		
 
 
-testAjouter = ajouter (test) "ok"
 
-testAjouter2 = ajouter (APREF[]) []
+
+
+
+
+
+
+
+testCodeOf = codeOf (3,APREF [('c',1,APREF [('a',-1,APREF [('b',2,APREF [])]),('b',-1,APREF [('b',2,APREF [])])])]) "cas"
+
+
+
+
+
+
+
+
+
+
+
+ajouterArbre (APREF arbre) lettres code = 
+		case lettres of
+			(x:[]) -> if (add arbre x) == []
+				  then APREF[(x,code,APREF[])] 
+				  else APREF[(\(a,b,c) -> (a,code,c)) (head $ add arbre x)]
+
+			(x:xs) -> if (add arbre x) == []
+				  then APREF[(x,code,ajouterArbre (APREF[]) xs code)]
+				  else APREF[((\(a,b,c) -> a) $ head ( add arbre x),(\(a,b,c) -> b) $ head ( add arbre x),(ajouterArbre ((\(a,b,c) -> c) $ head ( add arbre x)) xs code))]
+		where 
+		-- parcourir x pour trouver si la lettre existe déjà : si filter trouve un élément alors renvoyer la liste de cet élément sinon insérer
+		add tree lett = filter (\(cara,code,arbreSuivant) -> cara == lett) tree
+
+
